e0dd2f72d390e7916925fe03087fbc89536f0978
Gluhov Kirill <kirill.gluhov2003@yandex.ru>
2022-03-30 14:35:25 +0300

    .

--- esp-test.py
+++ esp-test.py
@@ -1,18 +1,19 @@
 #  pip install psycopg2
 # sudo apt-get install python3-psycopg2
+# pip install psycopg2-binary
 import psycopg2
 
 
 
 
 
-connect_str = "dbname='esp_db' user='esp_user' host='localhost' password='mypwd'"
+connect_str = "dbname='iu25' user='airflow' host='172.18.0.3' password='airflow'"
     # use our connection values to establish a connection
 conn = psycopg2.connect(connect_str)
     
 
 cur = conn.cursor()
-cur.execute("select * from coursework.users")
+cur.execute("select * from public.logistics")
 rows = cur.fetchall()
 print(rows)
 conn.close()
